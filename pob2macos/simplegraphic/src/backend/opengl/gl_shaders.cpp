f93a5b8acb936ed0d31b5cb5cd5ef4683f0b60d9
kokagex <k.kokage@gmail.com>
2026-02-11 07:32:43 +0900

    chore: limit tracking to PathOfBuilding.app bundle

--- pob2macos/simplegraphic/src/backend/opengl/gl_shaders.cpp
+++
@@ -1,8 +0,0 @@
-/*
- * SimpleGraphic - OpenGL Shaders
- * GLSL shader compilation (stub)
- */
-
-#include "sg_internal.h"
-
-// OpenGL shader management - to be implemented later
--- pob2macos/simplegraphic/src/rendering/sg_renderer.cpp
+++
@@ -1,9 +0,0 @@
-/*
- * SimpleGraphic - Renderer Interface
- * Abstract renderer implementation
- */
-
-#include "sg_internal.h"
-#include <stdio.h>
-
-// Stub implementations for MVP - to be filled in later
